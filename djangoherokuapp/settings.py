7ce29ec74a8a7875a8c2978f7a2b5ef0c280354f
Gokulkrishna Jayachandran <gokulkrishnajaya@gmail.com>
2020-04-15 23:52:09 +1000

    Initial Commit

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ekx@1w6r_wrw8##d(b_(heqdb78-)#1$i+a@a7wsa8z^&w(9)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['poisense.tech']
+ALLOWED_HOSTS = ['poisenseheroku.herokuapp.com']
+
 
 
 # Application definition
@@ -135,8 +139,6 @@ STATICFILES_DIRS = (
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-import dj_database_url
+
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-ALLOWED_HOSTS = ['poisenseheroku.herokuapp.com']
